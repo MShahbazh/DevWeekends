3ccd3935a41c1f23f64675f8a391e2ed57e62ece
MShahbazh <msh23904t@gmail.com>
2026-08-07 04:22:03 +0500

    Added

--- 6 Hooks + Weather + FoodRecipe/FoodRecipe/src/Router/RouterConfig.jsx
+++ 6 Hooks + Weather + FoodRecipe/FoodRecipe/src/Router/RouterConfig.jsx
@@ -3,7 +3,7 @@ import Details from "../components/Details/Details";
 import Favourites from "../components/Favourites/Favourites";
 import NavBar from "../components/NavBar/NavBar";
 import {
-  createBrowserRouter,
+  createHashRouter,
   createRoutesFromElements,
   Outlet,
   Route,
@@ -18,7 +18,7 @@ function Layout() {
   );
 }
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="" element={<Home/>}/>
