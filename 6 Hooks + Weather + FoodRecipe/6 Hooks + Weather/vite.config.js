4a375b463abe47a8ec32edce130a97d94a8c964b
MShahbazh <msh23904t@gmail.com>
2026-08-07 03:59:15 +0500

    Add landing page

--- 6 Hooks + Weather + FoodRecipe/6 Hooks + Weather/vite.config.js
+++ 6 Hooks + Weather + FoodRecipe/6 Hooks + Weather/vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/DevWeekends/'
+  base: '/DevWeekends/weather'
 })
