cc7d2407da11b63558121a13cc17a3d44ce8ddb1
MShahbazh <msh23904t@gmail.com>
2026-08-15 03:35:24 +0500

    FIxed

--- authRedux/src/main.jsx
+++ authRedux/src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import router from './Router/RouterConfig'
-import { RouterProvider } from 'react-router-dom'
+import { RouterProvider,HashRouter } from 'react-router-dom'
 import store from './ReduxStore/Store'
 import {Provider} from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
-    <RouterProvider router={router}>
-    </RouterProvider>
+    <HashRouter router={router}>
+    </HashRouter>
     </Provider>
 )
